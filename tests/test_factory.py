3680971fee2874cfeb459f2f2d92d8f8853659e5
cld <huey910@gmail.com>
2018-06-21 18:51:28 +0100

    add test

---
+++ tests/test_factory.py
@@ -0,0 +1,11 @@
+from medileaks import create_app
+
+
+def test_config():
+    assert not create_app().testing
+    assert create_app({'TESTING': True}).testing
+
+
+def test_hello(client):
+    response = client.get('/home')
+    assert response.data == b'Welcome to Medileaks!'
