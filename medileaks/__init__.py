fd8013e9e61fe00f54cc2825b98e9be1b57afbb9
cld <huey910@gmail.com>
2018-08-22 19:35:20 +0100

    Add todo list.

--- medileaks/__init__.py
+++ medileaks/__init__.py
@@ -1,23 +1,32 @@
+# -*- coding: utf-8 -*-
+"""Medileaks app and app method definitions.
+    
+   Todo:
+       1. Replace MongoDB and sql with elasticsearch.
+       2. Add remaining app methods.
+       
+"""
+
 import os
 
 from flask import Flask, render_template
+from flask_pymongo import PyMongo
+
 
 def create_app(test_config=None):
-    # create and configure the app
     app = Flask(__name__, instance_relative_config=True)
+    app.config["MONGO_URI"] = "mongodb://localhost:27017/myDatabase" 
+    mongo = PyMongo(app)
+
     app.config.from_mapping(
         SECRET_KEY='dev', 
-        DATABASE=os.path.join(app.instance_path, 'medileaks.sqlite'),
     )
 
     if test_config is None:
-        # load the instance config, if it exists, when not testing
         app.config.from_pyfile('config.py', silent=True)
     else:
-        # load the test config if passed in
         app.config.from_mapping(test_config)
 
-    # ensure the instance folder exists
     try:
         os.makedirs(app.instance_path)
     except OSError:
@@ -25,7 +34,7 @@ def create_app(test_config=None):
 
 
     @app.route('/home')
-    def welcome_message():
+    def home():
         return render_template('home.html')
 
     from . import db
@@ -35,4 +44,3 @@ def create_app(test_config=None):
     app.register_blueprint(auth.bp)
 
     return app
-
