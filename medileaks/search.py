da6a8108f76499f3b8ef9103aaf88cacdf0f9e19
cld <huey910@gmail.com>
2018-08-25 21:05:12 +0100

    Fix search view function and delete results view function.

---
+++ medileaks/search.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+"""User inputs medical procedure and is returned
+    1. summary of best practices.
+    2. option to view full version of best practices.
+    3. option to view full list of source urls.
+
+    Todo:
+        1. Fix import statements.
+        2. Maybe don't use flash(error).
+        3. How to link request forms to the UI?
+
+"""
+
+import functools
+
+from flask import (
+    flash, Blueprint, g, redirect, render_template, request, session, url_for
+)
+
+from spider.spider import get_best 
+
+from summariser.extractive_summariser import summarise 
+
+bp = Blueprint('search', __name__, url_prefix='/search')
+
+@bp.route('/search', methods=('GET', 'POST'))
+def search():
+    if request.method == 'POST': 
+        medical_procedure = request.form['procedure']
+        error = None
+    
+        if medical_procedure is None:
+            error = 'Please input a medical procedure.'
+        else:
+            content, sources = get_best(medical_procedure) 
+            if content is None: 
+                error = "No results found for {}.".format(medical_procedure)
+        
+        if error is None:
+            summary = summarise(content)
+
+        flash(error)
+
+    return render_template('search.html', result=[content, summary, sources])
